c36c20b269b48fe989d598434fac462963189bb4
Franckyroquai <franckyroquai@gmail.com>
2024-05-20 16:25:12 +0200

    utilisation short open tags

--- blog/templates/homepage.php
+++ blog/templates/homepage.php
@@ -15,20 +15,20 @@
             ?>
             <div class="news">
                 <h3>
-                    <?php echo htmlspecialchars($post['title']); ?>
+                    <?= htmlspecialchars($post['title']); ?>
                     <em>le <?php echo $post['french_creation_date']; ?></em>
                 </h3>
                 <p>
-                    <?php
+                    
                     // On affiche le contenu du billet
-                    echo nl2br(htmlspecialchars($post['content']));
+                   <?= nl2br(htmlspecialchars($post['content']));
                     ?>
                     <br />
                     <em><a href="#">Commentaires</a></em>
                 </p>
             </div>
             <?php
-        } // Fin de la boucle des billets
+        } // End of the loop of the posts
         ?>
     </body>
 </html>
