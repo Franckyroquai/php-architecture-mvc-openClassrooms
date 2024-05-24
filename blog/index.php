aefeaae4f2fb9479100076888c9ceeb9f3d725ba
Franckyroquai <franckyroquai@gmail.com>
2024-05-24 15:30:18 +0200

    utilisation des objets créés

--- blog/index.php
+++ blog/index.php
@@ -4,32 +4,36 @@ require_once('src/controllers/add_comment.php');
 require_once('src/controllers/homepage.php');
 require_once('src/controllers/post.php');
 
+use Application\Controllers\AddComment\AddComment;
+use Application\Controllers\Homepage\Homepage;
+use Application\Controllers\Post\Post;
+
 try {
-	if (isset($_GET['action']) && $_GET['action'] !== '') {
-	if ($_GET['action'] === 'post') {
-    	if (isset($_GET['id']) && $_GET['id'] > 0) {
-        	$identifier = $_GET['id'];
+    if (isset($_GET['action']) && $_GET['action'] !== '') {
+        if ($_GET['action'] === 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
 
-        	post($identifier);
-    	} else {
-        	throw new Exception('Aucun identifiant de billet envoyé');
-    	}
-	} elseif ($_GET['action']=== 'addComment') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            $identifier = $_GET['id'];
+                (new Post())->execute($identifier);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        } elseif ($_GET['action'] === 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
 
-            addComment($identifier, $_POST);
+                (new AddComment())->execute($identifier, $_POST);
+            } else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
         } else {
-            throw new Exception('Aucun identifiant de billet envoyé');
+            throw new Exception("La page que vous recherchez n'existe pas.");
         }
     } else {
-    	throw new Exception ("La page que vous recherchez n'existe pas.");
-	}
-} else {
-	homepage();
-}
+        (new Homepage())->execute();
+    }
 } catch (Exception $e) {
-	$errorMessage = $e->getMessage();
+    $errorMessage = $e->getMessage();
 
-	require('templates/error.php');
+    require('templates/error.php');
 }
