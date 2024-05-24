147e2aeb9d8b8826ae724bf7eb46b21e3449b38a
Franckyroquai <franckyroquai@gmail.com>
2024-05-24 16:05:11 +0200

    format

--- blog/templates/error.php
+++ blog/templates/error.php
@@ -2,7 +2,7 @@
 
 <?php ob_start(); ?>
 <h1>Le super blog de l'AVBN !</h1>
-<p>Une erreur est survenue: <?= $errorMessage ?></p>
+<p>Une erreur est survenue : <?= $errorMessage ?></p>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('layout.php') ?>
